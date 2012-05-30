1396993c4330ea0220f5240a3444c99aded4f5f1
Srdjan Stojiljkovic <srdjan@inchoo.net>
2012-05-30 11:16:15 +0200

    Trying to remove some files from github

--- app/code/community/Inchoo/Unicache/Model/Mysql4/unicache.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Inchoo_Unicache_Model_Mysql4_Unicache extends Mage_Core_Model_Mysql4_Abstract{
-	
-	public function _construct(){
-		$this->_init('unicache/unicache', 'unicache_id');
-	}
-	
-}
--- app/code/community/Inchoo/Unicache/Model/unicache.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Inchoo_Unicache_Model_Unicache extends Mage_Core_Model_Abstract{
-	
-	public function _construct(){
-		parent::_construct();
-		$this->_init('unicache/unicache');
-	}
-	
-}
